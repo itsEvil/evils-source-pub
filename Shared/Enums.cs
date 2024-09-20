35adfe7d15110e66ec75fab6088abc8aee346183
itsEvil <56killer65@gmail.com>
2024-09-20 02:08:07 +0100

    Renamed folder

---
+++ Shared/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared;
+
+public enum Rank {
+    None = 0,
+    Admin = 100,
+    Owner = 1000,
+}
